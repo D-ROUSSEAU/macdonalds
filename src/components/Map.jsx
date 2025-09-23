8f6063862913db64361eb058eb2be6e79693ad3f
Damian <d.rousseau-pro@protonmail.com>
2025-09-23 12:09:24 +0200

    Update leaflet marker img

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,17 @@ import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import { useDispatch, useSelector } from "react-redux"
 import { updateResults } from "../searchSlice"
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 function Map() {
     const dispatch = useDispatch()
